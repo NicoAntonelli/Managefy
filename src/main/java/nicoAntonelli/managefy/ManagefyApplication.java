3db4ba99ff4faf93416477a2362855e149b0e48a
NicoAntonelli <niconelli2@gmail.com>
2024-04-24 01:08:28 -0300

    App: demo base endpoint

--- src/main/java/nicoAntonelli/managefy/ManagefyApplication.java
+++ src/main/java/nicoAntonelli/managefy/ManagefyApplication.java
@@ -2,12 +2,19 @@ package nicoAntonelli.managefy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ManagefyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagefyApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello world";
+	}
 }
