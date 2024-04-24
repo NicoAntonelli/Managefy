e60c4e74200a1c9e2c3bc7eaf2d098aeb0a33f30
NicoAntonelli <niconelli2@gmail.com>
2024-04-23 21:15:20 -0300

    Add 'Spring Initializr' template (Spring Boot and Maven)

---
+++ src/main/java/nicoAntonelli/managefy/ManagefyApplication.java
@@ -0,0 +1,13 @@
+package nicoAntonelli.managefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagefyApplication.class, args);
+	}
+
+}
