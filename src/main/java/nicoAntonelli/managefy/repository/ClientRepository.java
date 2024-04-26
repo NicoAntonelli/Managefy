8dda0e5df7b20e15fd39f31c4dbd18c1d8838423
NicoAntonelli <niconelli2@gmail.com>
2024-04-25 21:21:14 -0300

    Add repository interface for client entity

---
+++ src/main/java/nicoAntonelli/managefy/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package nicoAntonelli.managefy.repository;
+
+import nicoAntonelli.managefy.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
--- src/main/java/nicoAntonelli/managefy/service/ClientService.java
+++ src/main/java/nicoAntonelli/managefy/service/ClientService.java
@@ -1,15 +1,22 @@
 package nicoAntonelli.managefy.service;
 
 import nicoAntonelli.managefy.entities.Client;
+import nicoAntonelli.managefy.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ClientService {
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
     public List<Client> GetClients() {
-        Client client1 = new Client(1L, "Nick", "Food & soda", "nick@mail.com", "123456", null);
-        Client client2 = new Client(2L, "Kasp", "Red meat", "kasp@mail.com", "111222", null);
-        return List.of(client1, client2);
+        return clientRepository.findAll();
     }
 }
