d183cbcb59ab64b3591b7cf168a3e3d3295fd3ac
NicoAntonelli <niconelli2@gmail.com>
2024-04-26 01:20:46 -0300

    Complete basic CRUD for client entity

--- src/main/java/nicoAntonelli/managefy/api/ClientController.java
+++ src/main/java/nicoAntonelli/managefy/api/ClientController.java
@@ -3,9 +3,7 @@ package nicoAntonelli.managefy.api;
 import nicoAntonelli.managefy.entities.Client;
 import nicoAntonelli.managefy.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,26 @@ public class ClientController {
     @GetMapping
     public List<Client> GetClients() {
         return clientService.GetClients();
+    }
+
+    @GetMapping(path = "{clientID}")
+    public Client GetOneClient(@PathVariable("clientID") Long clientID) {
+        return clientService.GetOneClient(clientID);
+    }
+
+    @PostMapping
+    public Client CreateClient(@RequestBody Client client) {
+        return clientService.CreateClient(client);
+    }
+
+    @PutMapping
+    public Client UpdateClient(@RequestBody Client client) {
+        return clientService.UpdateClient(client);
+    }
+
+    @DeleteMapping(path = "{clientID}")
+    public Client DeleteClient(@PathVariable("clientID") Long clientID)
+    {
+        return clientService.DeleteClient(clientID);
     }
 }
--- src/main/java/nicoAntonelli/managefy/services/ClientService.java
+++ src/main/java/nicoAntonelli/managefy/services/ClientService.java
@@ -5,7 +5,9 @@ import nicoAntonelli.managefy.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -18,5 +20,36 @@ public class ClientService {
 
     public List<Client> GetClients() {
         return clientRepository.findAll();
+    }
+
+    public Client GetOneClient(Long clientID) {
+        Optional<Client> client = clientRepository.findById(clientID);
+        if (client.isEmpty()) {
+            throw new IllegalStateException("Client with ID: " + clientID + " doesn't exists");
+        }
+
+        return client.get();
+    }
+
+    public Client CreateClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public Client UpdateClient(Client client) {
+        boolean exists = clientRepository.existsById(client.getId());
+        if (!exists) {
+            throw new IllegalStateException("Client with ID: " + client.getId() + " doesn't exists");
+        }
+
+        return clientRepository.save(client);
+    }
+
+    // Logic deletion (field: deletion date)
+    public Client DeleteClient(Long clientID) {
+        Client client = GetOneClient(clientID);
+        client.setDeletionDate(new Date());
+        clientRepository.save(client);
+
+        return client;
     }
 }
