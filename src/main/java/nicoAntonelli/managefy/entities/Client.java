e8cb3c41bd2e5ba1882135455af6d82978cf5d72
NicoAntonelli <niconelli2@gmail.com>
2024-04-28 20:10:36 -0300

    Use Lombok annotations to auto-generate constructors, getters, setters, toString, etc...

--- src/main/java/nicoAntonelli/managefy/entities/Client.java
+++ src/main/java/nicoAntonelli/managefy/entities/Client.java
@@ -1,10 +1,13 @@
 package nicoAntonelli.managefy.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.Date;
 
 @Entity
 @Table(name = "clients")
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Client {
     @Id
     @SequenceGenerator(
@@ -29,18 +32,6 @@ public class Client {
     @Column(nullable = true)
     private Date deletionDate;
 
-    public Client() { }
-
-    public Client(Long id, String name, String description, String mail,
-                  String phone, Date detelionDate) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.mail = mail;
-        this.phone = phone;
-        this.deletionDate = detelionDate;
-    }
-
     public Client(String name, String description, String mail,
                   String phone, Date detelionDate) {
         this.name = name;
@@ -48,65 +39,5 @@ public class Client {
         this.mail = mail;
         this.phone = phone;
         this.deletionDate = detelionDate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Date getDeletionDate() {
-        return deletionDate;
-    }
-
-    public void setDeletionDate(Date deletionDate) {
-        this.deletionDate = deletionDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", mail='" + mail + '\'' +
-                ", phone='" + phone + '\'' +
-                ", deletionDate=" + deletionDate +
-                '}';
     }
 }
