ca13ded0792799213d2f218440f449a05f4e17a3
NicoAntonelli <niconelli2@gmail.com>
2024-05-03 20:44:01 -0300

    Repositories: fix inline close-bracket

--- src/main/java/nicoAntonelli/managefy/repositories/UserRoleRepository.java
+++ src/main/java/nicoAntonelli/managefy/repositories/UserRoleRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleKey> {
-}
+public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleKey> { }
