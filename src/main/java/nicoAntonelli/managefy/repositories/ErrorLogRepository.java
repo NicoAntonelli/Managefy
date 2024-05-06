886f82905be105da4714cd21f5a0fe0451ebc9bf
NicoAntonelli <niconelli2@gmail.com>
2024-05-05 22:34:01 -0300

    ErrorLogRepository: add query with time-intervals

--- src/main/java/nicoAntonelli/managefy/repositories/ErrorLogRepository.java
+++ src/main/java/nicoAntonelli/managefy/repositories/ErrorLogRepository.java
@@ -2,7 +2,16 @@ package nicoAntonelli.managefy.repositories;
 
 import nicoAntonelli.managefy.entities.ErrorLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
-public interface ErrorLogRepository extends JpaRepository<ErrorLog, Long> { }
+public interface ErrorLogRepository extends JpaRepository<ErrorLog, Long> {
+    @Query("SELECT e " +
+           "FROM ErrorLog e " +
+           "WHERE e.origin = ?1 AND e.date >= ?2 AND e.date <= ?3")
+    List<ErrorLog> findByOrigin(String origin, Date initialDate, Date finalDate);
+}
