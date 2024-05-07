7c4591b8427ae2134d40598f8bd807a8621eb497
NicoAntonelli <niconelli2@gmail.com>
2024-05-07 01:21:38 -0300

    Business: set custom response, try-catch and error logs into controller (erase from service)

--- src/main/java/nicoAntonelli/managefy/api/BusinessController.java
+++ src/main/java/nicoAntonelli/managefy/api/BusinessController.java
@@ -2,7 +2,9 @@ package nicoAntonelli.managefy.api;
 
 import nicoAntonelli.managefy.entities.Business;
 import nicoAntonelli.managefy.entities.dto.BusinessWithUser;
+import nicoAntonelli.managefy.entities.dto.Result;
 import nicoAntonelli.managefy.services.BusinessService;
+import nicoAntonelli.managefy.services.ErrorLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,34 +14,96 @@ import java.util.List;
 @RequestMapping(path = "api/businesses")
 public class BusinessController {
     private final BusinessService businessService;
+    private final ErrorLogService errorLogService; // Dependency
 
     @Autowired
-    public BusinessController(BusinessService businessService) {
+    public BusinessController(BusinessService businessService, ErrorLogService errorLogService) {
         this.businessService = businessService;
+        this.errorLogService = errorLogService;
     }
 
     @GetMapping
-    public List<Business> GetBusinesses() {
-        return businessService.GetBusinesses();
+    public Result<List<Business>> GetBusinesses() {
+        try {
+            List<Business> businesses = businessService.GetBusinesses();
+            return new Result<>(businesses);
+        } catch (IllegalStateException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 400);
+        } catch (SecurityException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 401);
+        } catch (Exception ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 500);
+        }
     }
 
     @GetMapping(path = "{businessID}")
-    public Business GetOneBusiness(@PathVariable("businessID") Long businessID) {
-        return businessService.GetOneBusiness(businessID);
+    public Result<Business> GetOneBusiness(@PathVariable("businessID") Long businessID) {
+        try {
+            Business business = businessService.GetOneBusiness(businessID);
+            return new Result<>(business);
+        } catch (IllegalStateException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 400);
+        } catch (SecurityException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 401);
+        } catch (Exception ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 500);
+        }
     }
 
     @PostMapping
-    public Business CreateBusiness(@RequestBody BusinessWithUser businessWithUser) {
-        return businessService.CreateBusiness(businessWithUser);
+    public Result<Business> CreateBusiness(@RequestBody BusinessWithUser businessWithUser) {
+        try {
+            Business business = businessService.CreateBusiness(businessWithUser);
+            return new Result<>(business);
+        } catch (IllegalStateException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 400);
+        } catch (SecurityException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 401);
+        } catch (Exception ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 500);
+        }
     }
 
     @PutMapping
-    public Business UpdateBusiness(@RequestBody Business business) {
-        return businessService.UpdateBusiness(business);
+    public Result<Business> UpdateBusiness(@RequestBody Business business) {
+        try {
+            business = businessService.UpdateBusiness(business);
+            return new Result<>(business);
+        } catch (IllegalStateException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 400);
+        } catch (SecurityException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 401);
+        } catch (Exception ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 500);
+        }
     }
 
     @DeleteMapping(path = "{businessID}")
-    public Long BusinessIDUser(@PathVariable("businessID") Long businessID) {
-        return businessService.DeleteBusiness(businessID);
+    public Result<Long> BusinessIDUser(@PathVariable("businessID") Long businessID) {
+        try {
+            businessID = businessService.DeleteBusiness(businessID);
+            return new Result<>(businessID);
+        } catch (IllegalStateException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 400);
+        } catch (SecurityException ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 401);
+        } catch (Exception ex) {
+            errorLogService.SetBackendError(ex.getMessage());
+            return new Result<>(null, 500);
+        }
     }
 }
--- src/main/java/nicoAntonelli/managefy/services/BusinessService.java
+++ src/main/java/nicoAntonelli/managefy/services/BusinessService.java
@@ -18,104 +18,71 @@ public class BusinessService {
     private final BusinessRepository businessRepository;
     private final UserService userService; // Dependency
     private final UserRoleService userRoleService; // Dependency
-    private final ErrorLogService errorLogService; // Dependency
 
     @Autowired
     public BusinessService(BusinessRepository businessRepository,
                            UserService userService,
-                           UserRoleService userRoleService,
-                           ErrorLogService errorLogService) {
+                           UserRoleService userRoleService) {
         this.businessRepository = businessRepository;
         this.userService = userService;
         this.userRoleService = userRoleService;
-        this.errorLogService = errorLogService;
     }
 
     public List<Business> GetBusinesses() {
-        try {
-            return businessRepository.findAll();
-        } catch(Exception ex) {
-            errorLogService.SetBackendError(ex.getMessage());
-            return null;
-        }
+        return businessRepository.findAll();
     }
 
     public Boolean ExistsBusiness(Long businessID) {
-        try {
-            return businessRepository.existsById(businessID);
-        } catch(Exception ex) {
-            errorLogService.SetBackendError(ex.getMessage());
-            return null;
-        }
+        return businessRepository.existsById(businessID);
     }
 
     public Business GetOneBusiness(Long businessID) {
-        try {
-            Optional<Business> business = businessRepository.findById(businessID);
-            if (business.isEmpty()) {
-                throw new IllegalStateException("Error at 'GetOneBusiness' - Business with ID: " + businessID + " doesn't exist");
-            }
-
-            return business.get();
-        } catch(Exception ex) {
-            errorLogService.SetBackendError(ex.getMessage());
-            return null;
+        Optional<Business> business = businessRepository.findById(businessID);
+        if (business.isEmpty()) {
+            throw new IllegalStateException("Error at 'GetOneBusiness' - Business with ID: " + businessID + " doesn't exist");
         }
+
+        return business.get();
     }
 
     public Business CreateBusiness(BusinessWithUser businessWithUser) {
-        try {
-            // Validate associated user ID
-            boolean exists = userService.ExistsUser(businessWithUser.getUserID());
-            if (!exists) {
-                throw new IllegalStateException("Error at 'CreateBusiness' - User with ID: " + businessWithUser.getUserID() + " doesn't exist");
-            }
-
-            // Create business
-            Business business = businessWithUser.getBusiness();
-            business.setId(null);
-            business = businessRepository.save(business);
-
-            // Set associated user
-            User user = new User(businessWithUser.getUserID());
-
-            // Create user role
-            UserRole userRole = new UserRole(user, business, true, false, false);
-            userRoleService.CreateUserRole(userRole);
-
-            return business;
-        } catch(Exception ex) {
-            errorLogService.SetBackendError(ex.getMessage());
-            return null;
+        // Validate associated user ID
+        boolean exists = userService.ExistsUser(businessWithUser.getUserID());
+        if (!exists) {
+            throw new IllegalStateException("Error at 'CreateBusiness' - User with ID: " + businessWithUser.getUserID() + " doesn't exist");
         }
+
+        // Create business
+        Business business = businessWithUser.getBusiness();
+        business.setId(null);
+        business = businessRepository.save(business);
+
+        // Set associated user
+        User user = new User(businessWithUser.getUserID());
+
+        // Create user role
+        UserRole userRole = new UserRole(user, business, true, false, false);
+        userRoleService.CreateUserRole(userRole);
+
+        return business;
     }
 
     public Business UpdateBusiness(Business business) {
-        try {
-            boolean exists = ExistsBusiness(business.getId());
-            if (!exists) {
-                throw new IllegalStateException("Error at 'UpdateBusiness' - Business with ID: " + business.getId() + " doesn't exist");
-            }
-
-            return businessRepository.save(business);
-        } catch(Exception ex) {
-            errorLogService.SetBackendError(ex.getMessage());
-            return null;
+        boolean exists = ExistsBusiness(business.getId());
+        if (!exists) {
+            throw new IllegalStateException("Error at 'UpdateBusiness' - Business with ID: " + business.getId() + " doesn't exist");
         }
+
+        return businessRepository.save(business);
     }
 
     public Long DeleteBusiness(Long businessID) {
-        try {
-            boolean exists = ExistsBusiness(businessID);
-            if (!exists) {
-                throw new IllegalStateException("Error at 'DeleteBusiness' - Business with ID: " + businessID + " doesn't exist");
-            }
-
-            businessRepository.deleteById(businessID);
-            return businessID;
-        } catch(Exception ex) {
-            errorLogService.SetBackendError(ex.getMessage());
-            return null;
+        boolean exists = ExistsBusiness(businessID);
+        if (!exists) {
+            throw new IllegalStateException("Error at 'DeleteBusiness' - Business with ID: " + businessID + " doesn't exist");
         }
+
+        businessRepository.deleteById(businessID);
+        return businessID;
     }
 }
