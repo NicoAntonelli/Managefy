2a241af93ec7ee38014288f7975b0c4c1de34761
NicoAntonelli <niconelli2@gmail.com>
2024-05-09 01:06:08 -0300

    User: json ignore password

--- src/main/java/nicoAntonelli/managefy/entities/User.java
+++ src/main/java/nicoAntonelli/managefy/entities/User.java
@@ -26,6 +26,7 @@ public class User {
 
     @Column(nullable = false)
     private String mail; // Unique
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
     private String name; // Nullable
