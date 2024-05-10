af734f93fe9c2fbc34f0413d73218b12b7321615
NicoAntonelli <niconelli2@gmail.com>
2024-05-10 17:37:10 -0300

    Exceptions: make status a static property

--- src/main/java/nicoAntonelli/managefy/utils/Exceptions.java
+++ src/main/java/nicoAntonelli/managefy/utils/Exceptions.java
@@ -9,9 +9,9 @@ public class Exceptions {
     @Getter @NoArgsConstructor
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     static public final class BadRequestException extends RuntimeException {
+        static public final String status = "400 (Bad Request)";
         public String message;
-        public final String status = "400 (Bad Request)";
-        public Exception InnerException;
+        public Exception innerException;
 
         public BadRequestException(String message) {
             this.message = message;
@@ -19,16 +19,20 @@ public class Exceptions {
 
         public BadRequestException(String message, Exception ex) {
             this.message = message;
-            this.InnerException = ex;
+            this.innerException = ex;
+        }
+
+        public String getStatus() {
+            return status;
         }
     }
 
     @Getter @NoArgsConstructor
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     static public final class UnauthorizedException extends RuntimeException {
+        static public final String status = "401 (Unauthorized)";
         public String message;
-        public final String status = "401 (Unauthorized)";
-        public Exception InnerException;
+        public Exception innerException;
 
         public UnauthorizedException(String message) {
             this.message = message;
@@ -36,16 +40,20 @@ public class Exceptions {
 
         public UnauthorizedException(String message, Exception ex) {
             this.message = message;
-            this.InnerException = ex;
+            this.innerException = ex;
+        }
+
+        public String getStatus() {
+            return status;
         }
     }
 
     @Getter @NoArgsConstructor
     @ResponseStatus(HttpStatus.NOT_FOUND)
     static public final class NotFoundException extends RuntimeException {
+        static public final String status = "404 (Not Found)";
         public String message;
-        public final String status = "404 (Not Found)";
-        public Exception InnerException;
+        public Exception innerException;
 
         public NotFoundException(String message) {
             this.message = message;
@@ -53,16 +61,20 @@ public class Exceptions {
 
         public NotFoundException(String message, Exception ex) {
             this.message = message;
-            this.InnerException = ex;
+            this.innerException = ex;
+        }
+
+        public String getStatus() {
+            return status;
         }
     }
 
     @Getter @NoArgsConstructor
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     static public final class InternalServerErrorException extends RuntimeException {
+        static public final String status = "500 (Internal Server Error)";
         public String message;
-        public final String status = "500 (Internal Server Error)";
-        public Exception InnerException;
+        public Exception innerException;
 
         public InternalServerErrorException(String message) {
             this.message = message;
@@ -70,7 +82,11 @@ public class Exceptions {
 
         public InternalServerErrorException(String message, Exception ex) {
             this.message = message;
-            this.InnerException = ex;
+            this.innerException = ex;
+        }
+
+        public String getStatus() {
+            return status;
         }
     }
 }
