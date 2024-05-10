b460f41cc29f923e3d139fd6e6f7d2da404df35e
NicoAntonelli <niconelli2@gmail.com>
2024-05-10 00:01:25 -0300

    Custom exceptions: add optional inner exception

--- src/main/java/nicoAntonelli/managefy/utils/Exceptions.java
+++ src/main/java/nicoAntonelli/managefy/utils/Exceptions.java
@@ -11,9 +11,15 @@ public class Exceptions {
     static public final class BadRequestException extends RuntimeException {
         public String message;
         public final String status = "400 (Bad Request)";
+        public Exception InnerException;
 
         public BadRequestException(String message) {
             this.message = message;
+        }
+
+        public BadRequestException(String message, Exception ex) {
+            this.message = message;
+            this.InnerException = ex;
         }
     }
 
@@ -22,9 +28,15 @@ public class Exceptions {
     static public final class UnauthorizedException extends RuntimeException {
         public String message;
         public final String status = "401 (Unauthorized)";
+        public Exception InnerException;
 
         public UnauthorizedException(String message) {
             this.message = message;
+        }
+
+        public UnauthorizedException(String message, Exception ex) {
+            this.message = message;
+            this.InnerException = ex;
         }
     }
 
@@ -33,9 +45,15 @@ public class Exceptions {
     static public final class NotFoundException extends RuntimeException {
         public String message;
         public final String status = "404 (Not Found)";
+        public Exception InnerException;
 
         public NotFoundException(String message) {
             this.message = message;
+        }
+
+        public NotFoundException(String message, Exception ex) {
+            this.message = message;
+            this.InnerException = ex;
         }
     }
 
@@ -44,9 +62,15 @@ public class Exceptions {
     static public final class InternalServerErrorException extends RuntimeException {
         public String message;
         public final String status = "500 (Internal Server Error)";
+        public Exception InnerException;
 
         public InternalServerErrorException(String message) {
             this.message = message;
+        }
+
+        public InternalServerErrorException(String message, Exception ex) {
+            this.message = message;
+            this.InnerException = ex;
         }
     }
 }
