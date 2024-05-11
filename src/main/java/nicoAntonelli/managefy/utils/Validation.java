e4401de4401add9edfc45a698be86f3efe77c9c0
NicoAntonelli <niconelli2@gmail.com>
2024-05-11 17:17:35 -0300

    Validation: add non-capturing regex for primitives

--- src/main/java/nicoAntonelli/managefy/utils/Validation.java
+++ src/main/java/nicoAntonelli/managefy/utils/Validation.java
@@ -5,6 +5,12 @@ import java.util.regex.Pattern;
 
 @SuppressWarnings("BooleanMethodIsAlwaysInverted")
 public class Validation {
+    // Non-capturing regex for primitives
+    public static final String paramString  = "[a-zA-Z]+";
+    public static final String paramInteger = "[\\d]+";
+    public static final String paramDecimal = "(?:[0-9]*[.])?[0-9]+";
+    public static final String paramBoolean = "(?i)(?:true|false)";
+
     // At least one uppercase and lowercase letters, one digit and min 8 total length
     private static final Pattern passwordRegex = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$", Pattern.CASE_INSENSITIVE);
 
