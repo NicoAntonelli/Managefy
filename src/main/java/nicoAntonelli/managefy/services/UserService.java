49ac2867a7ec8c336cd3ecbf22e395cbc910ac6b
NicoAntonelli <niconelli2@gmail.com>
2024-05-12 13:02:46 -0300

    User: erase now-unused exists method

--- src/main/java/nicoAntonelli/managefy/services/UserService.java
+++ src/main/java/nicoAntonelli/managefy/services/UserService.java
@@ -42,10 +42,6 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public Boolean ExistsUser(Long userID) {
-        return userRepository.existsById(userID);
-    }
-
     public User GetOneUser(Long userID) {
         Optional<User> user = userRepository.findById(userID);
         if (user.isEmpty()) {
