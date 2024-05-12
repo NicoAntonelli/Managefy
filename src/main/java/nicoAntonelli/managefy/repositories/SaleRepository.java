8d4f8f6c8fe8cf0bacee161b5591f2d4f0d94dac
NicoAntonelli <niconelli2@gmail.com>
2024-05-12 20:10:04 -0300

    Sales: adapt repo queries to business validations, add a find query for incomplete sales

--- src/main/java/nicoAntonelli/managefy/repositories/SaleRepository.java
+++ src/main/java/nicoAntonelli/managefy/repositories/SaleRepository.java
@@ -7,11 +7,32 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
     @Query("SELECT s " +
             "FROM Sale s " +
-            "WHERE s.date >= ?1 AND s.date <= ?2")
-    List<Sale> findByInterval(LocalDateTime initialDate, LocalDateTime finalDate);
+            "INNER JOIN s.business b " +
+            "WHERE s.date >= ?1 AND s.date <= ?2" +
+            "AND s.state <> 'Cancelled' AND b.id = ?3")
+    List<Sale> findActivesByIntervalAndBusiness(Long businessID, LocalDateTime initialDate, LocalDateTime finalDate);
+
+    @Query("SELECT s " +
+            "FROM Sale s " +
+            "INNER JOIN s.business b " +
+            "WHERE (s.state = 'PendingPayment' OR s.state = 'PartialPayment') AND b.id = ?1")
+    List<Sale> findIncompleteByBusiness(Long businessID);
+
+    @Query("SELECT s " +
+            "FROM Sale s " +
+            "INNER JOIN s.business b " +
+            "WHERE s.id = ?1 AND s.state <> 'Cancelled' IS NULL AND b.id = ?2")
+    Optional<Sale> findByIdActiveAndBusiness(Long productID, Long businessID);
+
+    @Query("SELECT COUNT(s) > 0 " +
+            "FROM Sale s " +
+            "INNER JOIN s.business b " +
+            "WHERE s.id = ?1 AND s.state <> 'Cancelled' IS NULL AND b.id = ?2")
+    Boolean existsByIdActiveAndBusiness(Long productID, Long businessID);
 }
