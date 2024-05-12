a810ea19447ff7cb49bbb75ab5b5aeff91cbd6f2
NicoAntonelli <niconelli2@gmail.com>
2024-05-12 18:12:10 -0300

    Client service: fix grammar error in a comment

--- src/main/java/nicoAntonelli/managefy/services/ClientService.java
+++ src/main/java/nicoAntonelli/managefy/services/ClientService.java
@@ -1,7 +1,7 @@
 package nicoAntonelli.managefy.services;
 
 import jakarta.transaction.Transactional;
-import nicoAntonelli.managefy.entities.Client;
+import nicoAntonelli.managefy.entities.*;
 import nicoAntonelli.managefy.repositories.ClientRepository;
 import nicoAntonelli.managefy.utils.Exceptions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +9,24 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional
 public class ClientService {
     private final ClientRepository clientRepository;
+    private final BusinessService businessService; // Dependency
+    private final SaleService saleService; // Dependency
 
     @Autowired
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository,
+                         BusinessService businessService,
+                         SaleService saleService) {
         this.clientRepository = clientRepository;
+        this.businessService = businessService;
+        this.saleService = saleService;
     }
 
     public List<Client> GetClients() {
@@ -61,5 +69,29 @@ public class ClientService {
         clientRepository.save(client);
 
         return clientID;
+    }
+
+    private void ValidateSalesForClient(Client client, User user) {
+        // At least one sale
+        Set<Sale> sales = client.getSales();
+        if (sales == null || sales.isEmpty()) {
+            throw new Exceptions.BadRequestException("Error at 'ValidateSalesForClient' - Any sale was supplied");
+        }
+
+        // Sales need to exist, be associated with the user and each sale associated with the same business
+        Long businessID = 0L;
+        for (Product product : products) {
+            if (!productService.ExistsProduct(product, user)) {
+                throw new Exceptions.BadRequestException("Error at 'ValidateProductsForSupplier' - Product doesn't exist or it's not associated with the user: " + user.getId());
+            }
+
+            // First loop
+            if (businessID == 0L) businessID = product.getBusiness().getId();
+
+            // Check every product's businessID
+            if (!Objects.equals(businessID, product.getBusiness().getId())) {
+                throw new Exceptions.BadRequestException("Error at 'ValidateProductsForSupplier' - Each product needs to be associated with the same business");
+            }
+        }
     }
 }
