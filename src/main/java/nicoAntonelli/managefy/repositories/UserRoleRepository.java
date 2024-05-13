ea9465c3abb92d45c53be076562920f139076962
NicoAntonelli <niconelli2@gmail.com>
2024-05-12 23:38:06 -0300

    UserRole: add queries findByUser & findByBusiness in repo

--- src/main/java/nicoAntonelli/managefy/repositories/UserRoleRepository.java
+++ src/main/java/nicoAntonelli/managefy/repositories/UserRoleRepository.java
@@ -1,9 +1,26 @@
 package nicoAntonelli.managefy.repositories;
 
+import nicoAntonelli.managefy.entities.Business;
 import nicoAntonelli.managefy.entities.UserRole;
 import nicoAntonelli.managefy.entities.UserRoleKey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleKey> { }
+public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleKey> {
+    @Query("SELECT ur " +
+            "FROM UserRole ur " +
+            "INNER JOIN ur.user u " +
+            "WHERE u.id = ?1")
+    List<UserRole> findByUser(Long userID);
+
+    @Query("SELECT ur " +
+            "FROM UserRole ur " +
+            "INNER JOIN ur.business b " +
+            "WHERE b.id = ?1")
+    List<UserRole> findByBusiness(Long businessID);
+}
