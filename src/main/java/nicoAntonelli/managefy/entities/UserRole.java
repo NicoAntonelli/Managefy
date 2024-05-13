6f249eb6359284487ccb8bde990b7fc7a889acc4
NicoAntonelli <niconelli2@gmail.com>
2024-05-12 23:37:20 -0300

    UserRole: few improvements

--- src/main/java/nicoAntonelli/managefy/entities/UserRole.java
+++ src/main/java/nicoAntonelli/managefy/entities/UserRole.java
@@ -31,11 +31,11 @@ public class UserRole {
     private Business business;
 
     @Column(nullable = false)
-    private Boolean isManager;
+    private Boolean isManager = false;
     @Column(nullable = false)
-    private Boolean isAdmin;
+    private Boolean isAdmin = false;
     @Column(nullable = false)
-    private Boolean isCollaborator;
+    private Boolean isCollaborator = false;
 
     public UserRole(User user, Business business) {
         this.user = user;
@@ -48,17 +48,36 @@ public class UserRole {
         this.isCollaborator = isCollaborator;
     }
 
+    public UserRole(Long userID, Long businessID, String role) {
+        setUserByID(userID);
+        setBusinessByID(businessID);
+        if (!setRoleByText(role)) setRoleByText("Collaborator"); // Default
+    }
+
     public UserRoleKey getId() {
         return new UserRoleKey(getUser().getId(), getBusiness().getId());
     }
 
-    public void setBusinessByID(Long businessByID) {
-        business = new Business();
-        business.setId(businessByID);
+    public void setUserByID(Long userID) {
+        user = new User(userID);
     }
 
-    public void setUserByID(Long userID) {
-        user = new User();
-        user.setId(userID);
+    public void setBusinessByID(Long businessByID) {
+        business = new Business(businessByID);
+    }
+
+    public Boolean setRoleByText(String role) {
+        this.isManager = false;
+        this.isAdmin = false;
+        this.isCollaborator = false;
+
+        switch(role.toLowerCase()) {
+            case "manager" -> this.isManager = true;
+            case "admin" -> this.isAdmin = true;
+            case "collaborator" -> this.isCollaborator = true;
+            default -> { return false; }
+        }
+
+        return true;
     }
 }
