89928568f41df7d442e48045365ddef39418ad3a
NicoAntonelli <niconelli2@gmail.com>
2024-05-18 18:33:14 -0300

    DTOs: add comment for some attributes

--- src/main/java/nicoAntonelli/managefy/entities/dto/ClientCU.java
+++ src/main/java/nicoAntonelli/managefy/entities/dto/ClientCU.java
@@ -9,11 +9,11 @@ import java.util.Set;
 // Client for Create & Update
 @Data @NoArgsConstructor @AllArgsConstructor
 public class ClientCU {
-    private Long id;
+    private Long id; // Ignored for create
     private String name;
-    private String description;
-    private String email;
-    private String phone;
+    private String description; // Optional
+    private String email; // Optional
+    private String phone; // Optional
     private Long businessID;
     private Set<Long> salesIDs;
 }
--- src/main/java/nicoAntonelli/managefy/entities/dto/SupplierCU.java
+++ src/main/java/nicoAntonelli/managefy/entities/dto/SupplierCU.java
@@ -9,11 +9,11 @@ import java.util.Set;
 // Supplier for Create & Update
 @Data @NoArgsConstructor @AllArgsConstructor
 public class SupplierCU {
-    private Long id;
+    private Long id; // Ignored for create
     private String name;
-    private String description;
-    private String email;
-    private String phone;
+    private String description; // Optional
+    private String email; // Optional
+    private String phone; // Optional
     private Long businessID;
     private Set<Long> productsIDs;
 }
