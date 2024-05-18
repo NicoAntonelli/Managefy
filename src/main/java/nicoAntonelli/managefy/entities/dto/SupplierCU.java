c2e2a1fe2927e8b67a28c5699ad7321e00fa93e0
NicoAntonelli <niconelli2@gmail.com>
2024-05-18 14:15:04 -0300

    Supplier DTO: simplify

--- src/main/java/nicoAntonelli/managefy/entities/dto/SupplierCU.java
+++ src/main/java/nicoAntonelli/managefy/entities/dto/SupplierCU.java
@@ -3,14 +3,17 @@ package nicoAntonelli.managefy.entities.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import nicoAntonelli.managefy.entities.Supplier;
 
 import java.util.Set;
 
 // Supplier for Create & Update
 @Data @NoArgsConstructor @AllArgsConstructor
 public class SupplierCU {
-    private Supplier supplier;
+    private Long id;
+    private String name;
+    private String description;
+    private String email;
+    private String phone;
     private Long businessID;
-    private Set<Long> products;
+    private Set<Long> productsIDs;
 }
